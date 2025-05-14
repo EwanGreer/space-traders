641ebeb7bb2f857de2fbe771a13fc93921f9cf99
EwanGreer <ewanja.greer@gmail.com>
2025-05-14 23:10:25 +0100

    feat: add host to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.hosts << "rails-production-c9f1.up.railway.app"
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
