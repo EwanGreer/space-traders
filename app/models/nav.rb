c29031e1db0bbfe3b5653d6741193bfa56e442ec
EwanGreer <ewanja.greer@gmail.com>
2025-05-15 00:17:24 +0100

    feat: remove enums since they're broken for some reason

--- app/models/nav.rb
+++ app/models/nav.rb
@@ -1,19 +1,18 @@
 class Nav < ApplicationRecord
   belongs_to :ship
-
   has_one    :route, dependent: :destroy
 
-  enum status: {
-    IN_TRANSIT: "IN_TRANSIT",
-    DOCKED:     "DOCKED",
-    IN_ORBIT:   "IN_ORBIT"
-  }
-
-  enum flight_mode: {
-    CRUISE: "CRUISE",
-    STEALTH: "STEALTH",
-    BURN:    "BURN"
-  }
+  # enum status: {
+  #   in_transit: "in_transit",
+  #   docked:     "docked",
+  #   in_orbit:   "in_orbit"
+  # }
+  #
+  # enum flight_mode: {
+  #   cruise: "cruise",
+  #   stealth: "stealth",
+  #   burn:    "burn"
+  # }
 
   validates :system_symbol, :waypoint_symbol, :status, :flight_mode, presence: true
 end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,9 +1,9 @@
 class Registration < ApplicationRecord
   belongs_to :ship
 
-  enum role: {
-    FABRICATOR: "FABRICATOR"
-  }
+  # enum role: {
+  #   FABRICATOR: "FABRICATOR"
+  # }
 
   validates :name, :faction_symbol, :role, presence: true
 end
